07d9e6946c8bbf1f44e43037f713b956b77ecd79
Fullmetal235 <jjbillupsbrown@gmail.com>
2019-07-22 17:27:18 -0500

    Adding routes.

--- client/src/App.js
+++ client/src/App.js
@@ -15,7 +15,8 @@ const App = () => {
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      <Route path='/' exact component={MovieList}/>
+      <Route path='/movies/:id' component={Movie}/>
     </div>
   );
 };
